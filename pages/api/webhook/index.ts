ea61708e046aa37749b07761fcec1698269ebdf3
rakib <rahmed@hvac.mea.com>
2024-01-21 04:50:11 -0500

    Attempting Vercel Deploy Z

--- pages/api/webhook/index.ts
+++ pages/api/webhook/index.ts
@@ -1,17 +1,15 @@
-const { io } = require('../../../socket');
 import type { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === `POST`) {
     try {
       const webhook = req.body;
       console.log(`Webhook received:`, webhook);
-      io.emit('webhookEvent', webhook);
       res.status(200).json({ message: `Webhook received`, webhook });
     } catch (error) {
       res.status(500).json({ error: `Failed to get Webhook.` });
     }
   } else {
-    res.setHeader('Allow', ['POST']);
+    res.setHeader(`Allow`, [`POST`]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
